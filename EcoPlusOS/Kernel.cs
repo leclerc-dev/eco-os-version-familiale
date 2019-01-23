7d5a0d4804cb757877c99e15667a902513156c18
jeuxjeux20 Y <jeuxjeux20@outlook.fr>
2019-01-23 12:04:58 +0100

    Ajoutez des fichiers projet.

---
+++ EcoPlusOS/Kernel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sys = Cosmos.System;
+
+namespace EcoPlusOS
+{
+    public class Kernel : Sys.Kernel
+    {
+        protected override void BeforeRun()
+        {
+            var encoding = Sys.ExtendedASCII.CosmosEncodingProvider.Instance.GetEncoding(437);
+            Console.OutputEncoding = encoding;
+            Console.InputEncoding = encoding;
+            Console.Clear();
+            Console.WriteLine("Leclerc vous souhaite la bienvenue");
+        }
+
+        protected override void Run()
+        {
+            Console.Write("éco+> ");
+            var input = Console.ReadLine();
+            Console.Write("Oui j'adore le ");
+            Console.WriteLine(input);
+        }
+    }
+}
